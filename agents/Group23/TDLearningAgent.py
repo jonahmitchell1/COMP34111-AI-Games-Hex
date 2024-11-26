75c39fd7d823455893a0007b37cfb802e36204ee
aap9002 <aap9002@gmail.com>
2024-11-26 10:37:45 +0000

    add opponent player

--- agents/Group23/TDLearningAgent.py
+++ agents/Group23/TDLearningAgent.py
@@ -4,6 +4,7 @@ from src.AgentBase import AgentBase
 from src.Board import Board
 from src.Colour import Colour
 from src.Move import Move
+import copy
 
 
 class TDLearningAgent(AgentBase):
@@ -14,12 +15,14 @@ class TDLearningAgent(AgentBase):
     _learning_rate = 0.1
     _discount_rate = 0.9
     _value_table: dict[str, float]
+    _number_of_episodes = 10
 
     def __init__(self, colour: Colour):
         super().__init__(colour)
         self._choices = [
             (i, j) for i in range(self._board_size) for j in range(self._board_size)
         ]
+        self._value_table = {}
     
     def get_board_state_key(self, board: Board) -> str:
         """Get the key for the value table based on the given board state.
@@ -36,6 +39,20 @@ class TDLearningAgent(AgentBase):
         if key not in self._value_table:
             self._value_table[key] = 0
         return key
+    
+    def get_simulated_opp_move(self, board: Board, remaining_choices:list[Move]) -> Move:
+        """Get the simulated opponent move.
+
+        Args:
+            board (Board): The current board state
+
+        Returns:
+            Move: The simulated opponent move
+        """
+        # avoid modifying the original board passed by reference
+        move = choice(remaining_choices)
+        return Move(move[0], move[1])
+
 
     def make_move(self, turn: int, board: Board, opp_move: Move | None) -> Move:
         """Make a move chosen by TD learning.
@@ -56,26 +73,40 @@ class TDLearningAgent(AgentBase):
         if turn == 2:
             return Move(-1, -1) # swap move
 
+        for i in range(self._number_of_episodes):
+            # avoid modifying the original board passed by reference
+            board_copy:Board = copy.deepcopy(board)
+            remaining_moves_copy = self._choices.copy()
 
-        while True:
-            x, y = choice(self._choices) # random walk
-            
-            # init value table if unseen state
-            original_state_key = self.get_board_state_key(board)
+            while True:
+                x, y = choice(remaining_moves_copy) # random walk
+                
+                # init value table if unseen state
+                original_state_key = self.get_board_state_key(board_copy)
 
-            # make the move and get the new state
-            board.set_tile_colour(x,y, self.colour)
-            new_state_key = self.get_board_state_key(board)
+                # make the move and get the new state
+                board_copy.set_tile_colour(x,y, self.colour)
+                new_state_key = self.get_board_state_key(board_copy)
 
-            # quantify the reward
-            reward = self.get_state_value(board)
+                # remove the move from the remaining moves
+                remaining_moves_copy.remove((x,y))
 
-            # update the value table
-            self._value_table[original_state_key] = self._value_table[original_state_key] + self._learning_rate * (reward + (self._discount_rate * self._value_table[new_state_key]) - self._value_table[original_state_key])
+                # get opponent move
+                opp_move = self.get_simulated_opp_move(board_copy, remaining_moves_copy)
+                board_copy.set_tile_colour(opp_move.x, opp_move.y, self.opponent_colour)
 
-            # check if the game is over
-            if board.is_game_over():
-                break
+                # remove the move from the remaining moves
+                remaining_moves_copy.remove((opp_move.x,opp_move.y))
+
+                # quantify the reward
+                reward = self.get_state_value(board_copy)
+
+                # update the value table
+                self._value_table[original_state_key] = self._value_table[original_state_key] + self._learning_rate * (reward + (self._discount_rate * self._value_table[new_state_key]) - self._value_table[original_state_key])
+
+                # check if the game is over
+                if board_copy.is_game_over():
+                    break
 
         # choose the next move based on the value table
         best_move = None
